49d861dd314d013d3c96e5c5b15be9425f82f158
jimmykodes <jimmy@khanacademy.org>
2024-07-10 14:07:00 -0700

    feat: cache previously used args

--- lua/cheetah/code_actions/actions.lua
+++ lua/cheetah/code_actions/actions.lua
@@ -38,16 +38,40 @@ local function action(title, cmd)
 end
 
 local function action_with_args(title, cmd)
-	return {
-		title = title,
-		action = function()
-			vim.ui.input(
-				{ prompt = "Enter Args:" },
-				function(input)
+	local data_path = plenary.Path:new(vim.fn.stdpath('data'), "cheetah")
+	data_path:mkdir({ exists_ok = true })
+	local file_name = data_path:joinpath(vim.fn.sha256(cmd))
+	local actions = {
+		{
+			title = title,
+			action = function()
+				vim.ui.input({ prompt = "Enter Args:" }, function(input)
+					if input == nil then
+						return
+					end
+					file_name:write(input, 'w')
 					new_term({ cmd = string.format("%s %s", cmd, input) }):toggle()
 				end)
-		end
+			end
+		},
 	}
+	if file_name:exists() then
+		table.insert(actions, {
+			title = string.format("%s - cached", title),
+			action = function()
+				local contents = file_name:read()
+				vim.ui.input({ prompt = "Enter Args:", default = contents }, function(input)
+					if input == nil then
+						return
+					end
+					file_name:write(input, 'w')
+					new_term({ cmd = string.format("%s %s", cmd, input) }):toggle()
+				end)
+			end
+
+		})
+	end
+	return actions
 end
 
 function M.go(params)
@@ -61,30 +85,27 @@ function M.go(params)
 			return {
 				action("Go - run file", fileCmd),
 				action("Go - run module", modCmd),
-				action_with_args("Go - run file with args", fileCmd),
-				action_with_args("Go - run module with args", modCmd),
+				unpack(action_with_args("Go - run file with args", fileCmd)),
+				unpack(action_with_args("Go - run module with args", modCmd)),
 			}
 		end
 	end
 end
 
 function M.python(params)
-	for _, line in ipairs(params.content) do
-		if line == 'if __name__ == "__main__":' or line == "if __name__ == '__main__':" then
-			local fileCmd = string.format("python3 %s", params.bufname)
-			return {
-				action("Python - run file", fileCmd),
-				action_with_args("Python - run file with args", fileCmd),
-			}
-		end
-	end
+	local fileCmd = string.format("python3 %s", params.bufname)
+	local actions = {
+		action("Python - run file", fileCmd),
+		unpack(action_with_args("Python - run file with args", fileCmd)),
+	}
+	return actions
 end
 
 function M.lua(params)
 	local fileCmd = string.format("lua %s", params.bufname)
 	return {
 		action("Lua - run file", fileCmd),
-		action_with_args("Lua - run file with args", fileCmd),
+		unpack(action_with_args("Lua - run file with args", fileCmd)),
 	}
 end
 
